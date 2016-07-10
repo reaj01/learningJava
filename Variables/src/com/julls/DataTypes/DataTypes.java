aa6f7cc04e5908b3760464753e0d679fe0ae02cd
julia <jullskondratenko@gmail.com>
2016-07-10 21:03:06 +0300

    learned topic Primitive Data Types

---
+++ Variables/src/com/julls/DataTypes/DataTypes.java
@@ -0,0 +1,25 @@
+package com.julls.DataTypes;
+
+/**
+ * Created by julia on 10.07.16.
+ *
+ */
+public class DataTypes {
+    // there are 8 primitive data types
+
+    byte a = 12_7; // this is the max number for byte
+    short b = 3_2_7_6_7; // max number for short
+    int c = 65439_300; // int contains big values
+    long d = 77777; // more than int
+    float s = (float) 0.1;
+    double e = 5.674;
+    char r = 'R';
+    String s = "Idea is cool";
+
+    int a = _35678; // DON'T PUT _ BEFORE AND AFTER NUMBER or dot.
+    int b = 3_5_6_7_8; // PUT _ BETWEEN NUMBERS
+
+
+
+
+}
